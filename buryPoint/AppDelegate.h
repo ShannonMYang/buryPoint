0d8849ca89ea7bf751d875d99c2eb1fcc3c0b612
iosline <949022813@qq.com>
2018-08-28 21:54:18 +0800

    Initial Commit

---
+++ buryPoint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  buryPoint
+//
+//  Created by 贾永凯 on 2018/8/28.
+//  Copyright © 2018年 贾永凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ buryPoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  buryPoint
+//
+//  Created by 贾永凯 on 2018/8/28.
+//  Copyright © 2018年 贾永凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
